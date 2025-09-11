84d7c2268f5b62d7f62e96cb2689d98b08e8a484
muratanaoki <51051386+muratanaoki@users.noreply.github.com>
2025-09-12 01:17:33 +0900

    修正

--- src/pages/pronouns-test-page/utils/type.ts
+++ src/pages/pronouns-test-page/utils/type.ts
@@ -16,4 +16,16 @@ export type PronounItem = {
   ipa: string;
   /** 日本語訳 */
   jp: string;
+  /** 例文（中1レベル、英語のみ） */
+  exJ1?: string;
+  /** 例文（中2レベル、英語のみ） */
+  exJ2?: string;
+  /** 例文（中3レベル、英語のみ） */
+  exJ3?: string;
+  /** 例文（中1レベル）の日本語訳 */
+  exJ1Jp?: string;
+  /** 例文（中2レベル）の日本語訳 */
+  exJ2Jp?: string;
+  /** 例文（中3レベル）の日本語訳 */
+  exJ3Jp?: string;
 };
