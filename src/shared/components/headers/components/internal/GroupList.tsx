e0cdd741f10462a8f765ab2469057367e52bfd54
muratanaoki <51051386+muratanaoki@users.noreply.github.com>
2025-10-01 02:07:00 +0900

    修正

--- src/shared/components/headers/components/internal/GroupList.tsx
+++ src/shared/components/headers/components/internal/GroupList.tsx
@@ -4,7 +4,15 @@ import type { PosGroup } from '@/pages/pronouns-test-page/utils/domain/type';
 
 const formatNo = (idx: number) => `${String(idx + 1).padStart(2, '0')}.`;
 
-export const GroupList = ({ posKey, groups, onLinkClick }: { posKey: string; groups: PosGroup['groups']; onLinkClick: () => void }) => (
+export const GroupList = ({
+  posKey,
+  groups,
+  onLinkClick,
+}: {
+  posKey: string;
+  groups: PosGroup['groups'];
+  onLinkClick: () => void;
+}) => (
   <ul className={styles.subList} id={`nav-pos-${posKey}`}>
     {groups.map((g, idx) => (
       <li className={styles.subListItem} key={`${posKey}-${g.groupNo}`}>
